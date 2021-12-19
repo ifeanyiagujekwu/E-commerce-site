036d716ded6cf399598cd723da594d7c3e01d827
ifeanyi <ifeanyiagujekwu@gmail.com>
2021-12-19 08:10:05 +0100

    put content into the homepage and installed tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Homepage from './components/homepage.component';
 
 function App() {
-  return (
-    <div>
-      Hello world!
-    </div>
-  );
+  return <div>
+    <Homepage />
+  </div>;
 }
 
 export default App;
---
+++ src/components/homepage.component.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const Homepage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="menu-title">gadgets</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="menu-title">clothes</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="menu-title">clothing accessories</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="menu-title">visa procurement</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="menu-title">food</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="menu-title">houses</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="menu-title">Title</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="menu-title">cars</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="menu-title">home appliances</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Homepage
